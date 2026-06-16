28ccf35e3edf93b51f4ec95e1b120af52f9835a0
crybaby05 <benthiam750@gmail.com>
2026-06-16 16:37:24 +0000

    configuration de la classe personne

--- Entity/Personne.java
+++ Entity/Personne.java
@@ -2,4 +2,16 @@ package Entity;
 
 public class Personne {
     
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Personne(){}
+
+    public Personne(String nom, String prenom, String email){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
 }
