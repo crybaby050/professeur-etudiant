dbca75c0ebd0b4f82d96a2e0c72c12a70e9a2bd8
crybaby05 <benthiam750@gmail.com>
2026-06-16 18:29:23 +0000

    configuration des methodes pour le service des etudiants

--- Service/EtudiantService.java
+++ Service/EtudiantService.java
@@ -1,5 +1,27 @@
 package Service;
 
+import Entity.Etudiant;
+
 public class EtudiantService {
     
+    private Etudiant[] etudiants = new Etudiant[100];
+    private int nbreEtudiant = 0;
+
+    public void ajouterEtudiant(Etudiant etudiant) {
+
+        etudiants[nbreEtudiant] = etudiant;
+        nbreEtudiant++;
+    }
+
+    public void listerEtudiant(){
+        if(nbreEtudiant == 0){
+            System.out.println("Aucun etudiant enregistrer");
+        }else{
+            System.out.println("===============LISTE DES ETUDIANTS=============");
+            for (int i = 0; i < nbreEtudiant ; i++){
+                etudiants[i].afficherInfo();
+            }
+            System.out.println("===============FIN DE LISTE=============");
+        }
+    }
 }
