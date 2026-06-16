b96bd29ccc8edf676e229495fd4deb6b5e534d41
crybaby05 <benthiam750@gmail.com>
2026-06-16 20:08:09 +0000

    configuration de la class de service de professeur

--- Service/ProfesseurService.java
+++ Service/ProfesseurService.java
@@ -1,5 +1,26 @@
 package Service;
 
+import Entity.Professeur;
+
 public class ProfesseurService {
     
+    private Professeur[] professeurs = new Professeur[100];
+    private int nbreProf = 0;
+
+    public void ajouterEtudiant(Professeur professeur){
+        professeurs[nbreProf] = professeur;
+        nbreProf++;
+    }
+
+    public void listerProfesseur(){
+        if(nbreProf == 0){
+            System.out.println("Aucun professeur enregistrer");
+        }else{
+            System.out.println("===============LISTE DES ETUDIANTS=============");
+            for (int i = 0; i < nbreProf ; i++){
+                professeurs[i].afficherInfo();
+            }
+            System.out.println("===============FIN DE LISTE=============");
+        }
+    }
 }
