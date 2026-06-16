5a226e8aa31dc954e1c808892c6666d32f6c483c
crybaby05 <benthiam750@gmail.com>
2026-06-16 16:59:07 +0000

    redefinition de la fonction avec override

--- Entity/Etudiant.java
+++ Entity/Etudiant.java
@@ -5,8 +5,18 @@ public class Etudiant extends Personne{
     private String matricule;
     private String filiere;
 
-    public Etudiant(String matricule, String filiere){
+    public Etudiant(String nom, String prenom, String email, String matricule, String filiere){
+        super(nom, prenom, email);
         this.matricule = matricule;
         this.filiere = filiere;
+    }
+
+    @Override
+    public void afficherInfo(){
+        System.out.println("Nom : " + getNom());
+        System.out.println("Prenom : " + getPrenom());
+        System.out.println("Email : " + getEmail());
+        System.out.println("Matricule : "+ matricule);
+        System.out.println("Filiere : "+ filiere);
     }
 }
